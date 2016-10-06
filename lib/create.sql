2380f2084d8c03433357996c7d4ed51fd0197cfc
Peregrin <peregringaret@gmail.com>
2016-10-06 17:40:53 -0400

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,3 @@
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title STRING, category STRING, funding_goal INTEGER, start_date STRING, end_date STRING);
+CREATE TABLE users (id INTEGER PRIMARY KEY, name STRING, age INTEGER);
+CREATE TABLE pledges (id INTEGER PRIMARY KEY, amount INTEGER, user_ID INTEGER, project_ID INTEGER);
